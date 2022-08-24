d1bae1cc75293a1b373fed6c31bf4365bb6979ae
unknown <Niromedium@gmail.com>
2022-08-24 16:00:24 +0500

    add Задача6

---
+++ Case6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число:");
+double a = double.Parse(Console.ReadLine());
+
+if (a%2 == 0);
+{
+    Console.WriteLine("Было введено четное число");
+}
+if (a%2 > 0);
+{
+    Console.WriteLine("Было введено нечетное число");
+}
