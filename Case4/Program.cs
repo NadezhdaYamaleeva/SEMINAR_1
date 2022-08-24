f6a39caafc67fc15fc9f16d066bb3bb70f2257b3
unknown <Niromedium@gmail.com>
2022-08-24 11:19:10 +0500

    add Задача4

---
+++ Case4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число 1:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 3:");
+int c = int.Parse(Console.ReadLine());
+
+if (a > b); (a > c);
+{
+    Console.WriteLine("max = " + a);
+}
+if (b > a); (b > c);
+{
+    Console.WriteLine("max = " + b);
+}
+
+{
+    Console.WriteLine("max = " + c);
+}
