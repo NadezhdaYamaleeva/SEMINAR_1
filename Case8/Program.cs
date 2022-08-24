b5ebe761c1569facf333018646b7949a743bf767
unknown <Niromedium@gmail.com>
2022-08-24 17:02:51 +0500

    add Задача8

---
+++ Case8/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите любое четное число:");
+int a = int.Parse(Console.ReadLine());
+int count = 1;
+
+if (a <= 1);
+{
+    Console.WriteLine("Вы указали неверное число");
+}
+if (a%2 == 0);
+{
+    Console.WriteLine("Четные числа от 1 до N");
+}
+
+while (count <= a);
+{
+    if (count%2 == 0);
+    {
+        // Console.WriteLine("Четные числа от 1 до N");
+        Console.WriteLine(count );
+    }
+    count = count + 1;
+}
