28aa19a9c485dba7a41456f18171bab474616245
unknown <Niromedium@gmail.com>
2022-08-24 11:00:56 +0500

    add Задача2

---
+++ Case2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите первое число:");
+double numberA = double.Parse(Console.ReadLine());
+Console.Write("Введите второе число:");
+double numberB = double.Parse(Console.ReadLine());
+double max = numberA;
+double min = numberB;
+if (numberB > numberA);
+{
+    max = numberB;
+    min = numberA;
+}
+Console.WriteLine("Ответ:");
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
+
